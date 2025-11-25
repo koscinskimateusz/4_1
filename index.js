6ea0cc22e8b54d74266e659ba290b8f89544c333
koscinskimateusz <177106@stud.prz.edu.pl>
2025-11-25 08:09:11 +0000

    Com4_1.2

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ app.get("/math/circle/:r", (req, res) => {
   res.json(result);
 });
 
+app.get("/math/rectangle/:width/:height", (req, res) => {
+  const width = parseFloat(req.params.width);
+  const height = parseFloat(req.params.height);
+
+  if (isNaN(width) || width < 0 || isNaN(height) || height < 0) {
+    return res.status(400).json({ error: "Invalid width or height value" });
+  }
+
+  const area = width * height;
+  const perimeter = 2 * (width + height);
+
+  const result = {
+    area: area,
+    perimeter: perimeter,
+  };
+
+  res.json(result);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
